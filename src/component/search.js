740cead8ac98721d3078d0a06062255da6d5094d
poramin <614259039@webmail.npru.ac.th>
2021-03-17 03:06:09 +0700

    poramin

--- src/component/search.js
+++ src/component/search.js
@@ -11,7 +11,7 @@ const Search=()=>{
         setKeySearch(Event.target.value);
         console.log(keySearch);
     }
-    const handelEnterKeyPressed=()=>{
+    const handelEnter=()=>{
         const unsplash="http://api.unsplash.com/search/photos?page=1&query="+keySearch+"&client_id="+Access;
     axios.get(unsplash).then((response)=>{
         console.log(response);
@@ -22,7 +22,7 @@ const Search=()=>{
     return (
         <div>
             <br/>
-            <input class="search" type="text" onChange={handelInput} onKeyPress={handelEnterKeyPressed} value={keySearch}/> 
+            <input class="search" type="text" onChange={handelInput} onKeyPress={handelEnter}/> 
             
             <div className="gallery">
             {userphotos.map(items=>(
